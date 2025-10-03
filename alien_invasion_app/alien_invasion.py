18705c740475967273abd819de726cdd32cc571e
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-03 14:07:43 -0600

    refactor(bullet): Extract bullet update logic into a helper method

--- alien_invasion_app/alien_invasion.py
+++ alien_invasion_app/alien_invasion.py
@@ -30,12 +30,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
-
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self._update_bullets()
             self._update_screen()         
             self.clock.tick(60)
     
@@ -88,6 +83,16 @@ class AlienInvasion:
         """Create a new bullet and add it to the bullets group."""
         new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
