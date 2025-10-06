eff12eef26b275f21fdcdf5ffcf4d46e8dbbaeb4
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-05 20:57:28 -0600

    refactor(settings): Promote alien scale factor to a setting attribute

--- alien_invasion_app/settings.py
+++ alien_invasion_app/settings.py
@@ -17,10 +17,12 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+
+        # Alien settings
+        self.scale_factor = 0.2
     
     def resize_alien_image(self, original_size):
-        """Resize the alien image based on the given scale factor."""
-        scale_factor = 0.2
-        new_width = int(original_size[0] * scale_factor)
-        new_height = int(original_size[1] * scale_factor)
+        """Resize the alien image based on the scale factor."""
+        new_width = int(original_size[0] * self.scale_factor)
+        new_height = int(original_size[1] * self.scale_factor)
         return (new_width, new_height)
