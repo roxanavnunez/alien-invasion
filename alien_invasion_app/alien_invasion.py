ca28aa2f520e299c99b21dd781cf5ce3277b465a
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-07 11:58:30 -0600

    feat(gameplay): Create new fleet when all aliens are destroyed

--- alien_invasion_app/alien_invasion.py
+++ alien_invasion_app/alien_invasion.py
@@ -102,6 +102,11 @@ class AlienInvasion:
         # If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _create_alien(self, current_x, current_y):
         """Create an alien and place it in the row."""
