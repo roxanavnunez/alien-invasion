c3c3c186894aaee8cf9831c2b5521084b67aa89a
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-08 13:17:09 -0600

    feat(scoring): Award points for shooting aliens

--- alien_invasion_app/settings.py
+++ alien_invasion_app/settings.py
@@ -22,9 +22,6 @@ class Settings:
         self.alien_scale_factor = 0.13
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
@@ -45,6 +42,9 @@ class Settings:
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+        
+        # Scoring settings
+        self.alien_points = 50
     
     def increase_speed(self):
         """Increase speed settings."""
