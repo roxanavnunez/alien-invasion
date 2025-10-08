ddaad86c00ea1a34b4c8a53c8d82212aa341179d
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-08 11:09:23 -0600

    feat(ui): Hide mouse cursor during active gameplay

--- alien_invasion_app/alien_invasion.py
+++ alien_invasion_app/alien_invasion.py
@@ -87,7 +87,9 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.game_active = True
@@ -99,8 +101,10 @@ class AlienInvasion:
             # Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()
-        
 
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
+        
     def _toggle_fullscreen(self):
         """Toggle fullscreen mode and reposition the ship."""
         self.settings.fullscreen = not self.settings.fullscreen
@@ -230,6 +234,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
            
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
