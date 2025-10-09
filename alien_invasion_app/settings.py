ce600fac9bdc478e906e072f195bbb314790cc47
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-08 18:23:01 -0600

    chore(settings): Update screen dimensions

--- alien_invasion_app/settings.py
+++ alien_invasion_app/settings.py
@@ -5,7 +5,7 @@ class Settings:
         """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 750
         self.bg_color = (230, 230, 230)
         self.fullscreen = False
 
