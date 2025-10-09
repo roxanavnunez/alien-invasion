98c3d065e0d12ae1ca3a525b46fd9a2a7542b26e
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-08 21:47:12 -0600

    feat(bullet): Create AlienBullet class for enemy projectiles

---
+++ alien_invasion_app/alien_bullet.py
@@ -0,0 +1,19 @@
+import pygame
+from bullet import Bullet
+
+class AlienBullet(Bullet):
+    """A class to manage bullets fired from aliens."""
+    
+    def __init__(self, ai_game, alien):
+        """Create a bullet object at the alien's current position."""
+        super().__init__(ai_game)
+        self.rect.midbottom = alien.rect.midtop
+        self.y = float(self.rect.y)
+        self.color = (255, 0, 0)
+    
+    def update(self):
+        """Move the bullet down the screen."""
+        # Update the decimal position of the bullet.
+        self.y += self.speed
+        # Update the rect position.
+        self.rect.y = self.y
