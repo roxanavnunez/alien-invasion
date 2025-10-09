aa17abc9f53fd51ade26c2f9d158763ebec1d96a
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-08 20:46:43 -0600

    refactor(settings): Refactor bullet speed to a static setting and increase value

--- alien_invasion_app/settings.py
+++ alien_invasion_app/settings.py
@@ -16,8 +16,9 @@ class Settings:
         self.remain_ship_scale = 0.75
 
         # Bullet settings
-        self.bullet_width = 3
+        self.bullet_width = 3.5
         self.bullet_height = 15
+        self.bullet_speed = 5.0
         self.bullet_color = (60, 60, 60)
 
          # Alien settings
@@ -42,7 +43,7 @@ class Settings:
     def initialize_dynamic_settings(self): 
         """Initialize settings that change throughout the game."""
         
-        self.bullet_speed = 4.0
+        
         self.alien_speed = 1.0
 
         # fleet_direction of 1 represents right; -1 represents left.
